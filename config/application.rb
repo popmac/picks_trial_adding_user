ee48eef947fbea092183087a1c777e07be2551fa
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-16 17:35:12 +0900

    ロケールファイルのロードパスの設定を追加した。デフォルトロケールを日本語にした。

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module BackofficePicks
   class Application < Rails::Application
+    config.i18n.load_path +=
+      Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
