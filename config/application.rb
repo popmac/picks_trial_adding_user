a602464d551265ecf69900ae917b456ae3b7afbf
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-16 17:50:21 +0900

    ジェネレーターの設定を追加した。

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,9 @@ module BackofficePicks
     config.i18n.default_locale = :ja
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators do |g|
+      g.helper false
+      g.assets false
+    end
   end
 end
