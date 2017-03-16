757dca1d9c840c77f08bef3599ddb457b73209f0
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-16 17:58:26 +0900

    RSpecのためにジェネレーターの設定を追加した。

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,14 @@ module BackofficePicks
     config.generators do |g|
       g.helper false
       g.assets false
+      g.test_framework :rspec,
+      fixtures: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      controller_specs: true,
+      request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
   end
 end
