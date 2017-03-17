f0b3dd20eecf34abb541dcfaf05d8b1a2d87d99c
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-17 20:05:15 +0900

    アカウント作成に関するルーティングを追加した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   namespace :portal, path: '/' do
     root 'top#index'
+    get 'signup' => 'accounts#signup', as: :signup
+    resources :accounts, only: [ :new, :create ] do
+      patch :send_mail, on: :collection
+      get :after_send, on: :collection
+      get :thanks, on: :collection
+    end
   end
 end
