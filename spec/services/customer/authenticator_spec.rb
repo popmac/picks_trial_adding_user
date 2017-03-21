df2782de69951ba2f8fa82edd9fe1ae945deaf33
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-21 15:39:41 +0900

    リファクタリングをした。

--- spec/services/customer/authenticator_spec.rb
+++ spec/services/customer/authenticator_spec.rb
@@ -3,23 +3,23 @@ require 'rails_helper'
 RSpec.describe Customer::Authenticator do
   describe '#authenticate' do
     example '正しいパスワードならtrueを返す' do
-      m = build(:user)
-      expect(Customer::Authenticator.new(m).authenticate('password')).to be_truthy
+      u = build(:user)
+      expect(Customer::Authenticator.new(u).authenticate('password')).to be_truthy
     end
 
     example '誤ったパスワードならfalseを返す' do
-      m = build(:user)
-      expect(Customer::Authenticator.new(m).authenticate('xy')).to be_falsey
+      u = build(:user)
+      expect(Customer::Authenticator.new(u).authenticate('xy')).to be_falsey
     end
 
     example 'パスワードが未設定ならfalseを返す' do
-      m = build(:user, password: nil)
-      expect(Customer::Authenticator.new(m).authenticate(nil)).to be_falsey
+      u = build(:user, password: nil)
+      expect(Customer::Authenticator.new(u).authenticate(nil)).to be_falsey
     end
 
     example '停止フラグが立っていてもtrueを返す' do
-      m = build(:user, suspended: true)
-      expect(Customer::Authenticator.new(m).authenticate('password')).to be_truthy
+      u = build(:user, suspended: true)
+      expect(Customer::Authenticator.new(u).authenticate('password')).to be_truthy
     end
   end
 end
