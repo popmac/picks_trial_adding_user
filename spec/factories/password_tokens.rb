28e48f2caf67f768a04148328501d9bfa6de0be1
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-22 18:00:59 +0900

    "bundle exec annotate"を実行した。

--- spec/factories/password_tokens.rb
+++ spec/factories/password_tokens.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: password_tokens
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  value      :text(65535)      not null
+#  used       :boolean          default("0"), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :password_token do
     
