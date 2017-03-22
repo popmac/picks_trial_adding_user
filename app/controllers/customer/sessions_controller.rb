471b768ffc5c3397f24d66790bf0ad935364ce26
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-22 18:19:57 +0900

    Userのdeletedカラムの名前をdeleted_flagカラムに変更した。Userの"add_index :users, :email_for_index, unique: true"の記述を削除した。

--- app/controllers/customer/sessions_controller.rb
+++ app/controllers/customer/sessions_controller.rb
@@ -19,7 +19,7 @@ class Customer::SessionsController < Customer::Base
       if user.suspended?
         flash.alert = 'アカウントが停止されています'
         render action: 'new'
-      elsif user.deleted?
+      elsif user.deleted_flag?
         flash.alert = 'アカウントは削除済みです'
         render action: 'new'
       else
