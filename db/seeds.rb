fd440b591ebe9382c7caa57d444a4551369efaae
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-24 20:15:43 +0900

    シードを環境ごとに分けるようにした。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,25 @@
-table_names = %w(users administrators)
-table_names.each do |table_name|
-  path = Rails.root.join('db', 'seeds', Rails.env, "#{table_name}.rb")
-  if File.exist?(path)
-    puts "Creating #{table_name}..."
-    require(path)
+# development と staging のテーブル群
+if Rails.env.development? || Rails.env.staging?
+  table_names1 = %w(
+    users administrators
+  )
+  table_names1.each do |table_name|
+    path = Rails.root.join('db', 'seeds', 'development', "#{table_name}.rb")
+    if File.exist?(path)
+      puts "Creating #{table_name}...."
+      require(path)
+    end
+  end
+end
+
+# production のテーブル群
+if Rails.env.production?
+  table_names2 = %w(administrators)
+  table_names2.each do |table_name|
+    path = Rails.root.join('db', 'seeds', 'production', "#{table_name}.rb")
+    if File.exist?(path)
+      puts "Creating #{table_name}...."
+      require(path)
+    end
   end
 end
