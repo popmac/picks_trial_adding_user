2e88aec1b504d953a7a0fa79da475ecbfa2d1abe
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-25 20:25:59 +0900

    Mailerのプレビューのコードを修正した。

--- spec/mailers/previews/customer_mailer_preview.rb
+++ spec/mailers/previews/customer_mailer_preview.rb
@@ -3,7 +3,11 @@ class CustomerMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/customer_mailer/forgot_password
   def forgot_password
-    CustomerMailer.forgot_password
+    user = User.new
+    user.email = 'hoge1@hoge.com'
+    token = PasswordToken.new(id: 1)
+    token.value = PasswordToken.create_salt
+    CustomerMailer.forgot_password(user, token)
   end
 
 end
