82727267fd461d60dc06f29396b477e96556363e
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-27 18:46:12 +0900

    Articleモデルにcategory_idカラムを追加した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327073030) do
+ActiveRecord::Schema.define(version: 20170327094442) do
 
   create_table "account_email_tokens", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                                         null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170327073030) do
     t.boolean  "published",                 default: false, null: false
     t.datetime "created_at",                                null: false
     t.datetime "updated_at",                                null: false
+    t.integer  "category_id"
   end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
